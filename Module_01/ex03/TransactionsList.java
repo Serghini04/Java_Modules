04c6af7050c707894d19b2ea6520bf88de6b7fc9
Mehdi <mehdiaerghini@gmail.com>
2025-03-06 22:36:24 +0000

    Initial commit

---
+++ Module_01/ex03/TransactionsList.java
@@ -0,0 +1,8 @@
+import java.util.UUID;
+
+public interface TransactionsList
+{
+	public void	addTransaction(Transaction transaction);
+	public void removeTransaction(UUID id);
+	Transaction[] toArray();
+}
